f238011e96403da405d16e142ff5ee41d4e3c960
astarta16 <ngedeshuri2001@gmail.com>
2024-09-09 18:31:11 +0400

    Add

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ explosion_fx.set_volume(0.25)
 explosion2_fx = pygame.mixer.Sound("img/explosion2.wav")
 explosion2_fx.set_volume(0.25)
 
-# laser_fx = pygame.mixer.Sound("img/laser.wav")
-# laser_fx.set_volume(0.25)
+laser_fx = pygame.mixer.Sound("img/laser.wav")
+laser_fx.set_volume(0.25)
 
 
 # # თამაშის ცვლადები
