000eec1a007a15a595aae9a02373a2f9b7e86a11
astarta16 <ngedeshuri2001@gmail.com>
2024-09-10 10:44:10 +0400

    Add

--- main.py
+++ main.py
@@ -126,15 +126,15 @@ class Bullets(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
 
-#     def update(self):
-#         self.rect.y -= 5
-#         if self.rect.bottom < 0:
-#             self.kill()
-#         if pygame.sprite.spritecollide(self, alien_group, True):
-#             self.kill()
-#             explosion_fx.play()
-#             explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
-#             explosion_group.add(explosion)
+    def update(self):
+        self.rect.y -= 5
+        if self.rect.bottom < 0:
+            self.kill()
+        if pygame.sprite.spritecollide(self, alien_group, True):
+            self.kill()
+            explosion_fx.play()
+            explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
+            explosion_group.add(explosion)
 
 
 # Create Aliens class
