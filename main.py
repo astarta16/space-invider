1bd37b3a68b03850017c8dde1a326b3b1ae8d46f
astarta16 <ngedeshuri2001@gmail.com>
2024-09-10 10:43:26 +0400

    Add

--- main.py
+++ main.py
@@ -176,26 +176,26 @@ class Bullets(pygame.sprite.Sprite):
 #             explosion_group.add(explosion)
 
 
-# # Create Explosion class
-# class Explosion(pygame.sprite.Sprite):
-#     def __init__(self, x, y, size):
-#         pygame.sprite.Sprite.__init__(self)
-#         self.images = []
-#         for num in range(1, 6):
-#             img = pygame.image.load(f"img/exp{num}.png")
-#             if size == 1:
-#                 img = pygame.transform.scale(img, (20, 20))
-#             if size == 2:
-#                 img = pygame.transform.scale(img, (40, 40))
-#             if size == 3:
-#                 img = pygame.transform.scale(img, (160, 160))
-#             # Add the image to the list
-#             self.images.append(img)
-#         self.index = 0
-#         self.image = self.images[self.index]
-#         self.rect = self.image.get_rect()
-#         self.rect.center = [x, y]
-#         self.counter = 0
+# Create Explosion class
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, x, y, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for num in range(1, 6):
+            img = pygame.image.load(f"img/exp{num}.png")
+            if size == 1:
+                img = pygame.transform.scale(img, (20, 20))
+            if size == 2:
+                img = pygame.transform.scale(img, (40, 40))
+            if size == 3:
+                img = pygame.transform.scale(img, (160, 160))
+            # Add the image to the list
+            self.images.append(img)
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.counter = 0
 
     def update(self):
         explosion_speed = 3
