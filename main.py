41e7000e59368e2a70f9b42efae75c71f969ee06
nini gedeshuri <111783935+astarta16@users.noreply.github.com>
2024-09-11 15:32:30 +0400

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ laser_fx.set_volume(0.25)
 # თამაშის ცვლადები
 rows = 5
 cols = 5
-alien_cooldown = 1000  # Bullet cooldown in milliseconds
+alien_cooldown = 1000  
 last_alien_shot = pygame.time.get_ticks()  # Time when the last alien shot was fired
 countdown = 3  # Countdown timer before the game starts
 last_count = pygame.time.get_ticks()  # Time when the last countdown tick occurred
