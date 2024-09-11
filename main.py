dbc7074aabf560b8bc0fd8bd2c367ae6c75007e1
nini gedeshuri <111783935+astarta16@users.noreply.github.com>
2024-09-11 15:32:40 +0400

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ laser_fx.set_volume(0.25)
 rows = 5
 cols = 5
 alien_cooldown = 1000  
-last_alien_shot = pygame.time.get_ticks()  # Time when the last alien shot was fired
+last_alien_shot = pygame.time.get_ticks() 
 countdown = 3  # Countdown timer before the game starts
 last_count = pygame.time.get_ticks()  # Time when the last countdown tick occurred
 game_over = 0  # 0 means game is ongoing, 1 means player has won, -1 means player has lost
