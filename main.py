93aa4bb4e587285d7deeb00d50306284efee88ad
nini gedeshuri <111783935+astarta16@users.noreply.github.com>
2024-09-14 23:14:57 +0400

    Update main.py

--- main.py
+++ main.py
@@ -203,7 +203,6 @@ def create_aliens():
 create_aliens()  # Create the initial set of aliens
 
 
-# Create the player spaceship
 spaceship = Spaceship(int(screen_width / 2), screen_height - 100, 3)
 spaceship_group.add(spaceship)
 
